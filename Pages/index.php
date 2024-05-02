6692e88af30fb72e63d1a467e6e5db8d284a3e0c
RoshyBoshy <roshanvyas31@gmail.com>
2024-05-02 18:45:06 +0100

    changes to login page

--- Pages/index.php
+++ Pages/index.php
@@ -6,7 +6,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
-        href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Kanit:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Kanit:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Poetsen+One&display=swap"
         rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="../Stylesheets/Login.css">
 </head>
@@ -16,7 +16,6 @@
         <h1>Business.io</h1>
         <div class="form-container">
             <form class="form" action="login.php" method="post">
-                <h2>LOGIN</h2>
                 <?php if (isset($_GET['error'])) { ?>
                     <p class="error"> <?php echo $_GET['error']; ?></p>
                 <?php } ?>
