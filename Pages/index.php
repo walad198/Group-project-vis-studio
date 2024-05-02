af68c7e4a8eeb7a22a67e75436222aed41a8fb4e
RoshyBoshy <roshanvyas31@gmail.com>
2024-05-02 18:31:57 +0100

    Added more css and styling to login page

--- Pages/index.php
+++ Pages/index.php
@@ -2,7 +2,12 @@
 <html>
 
 <head>
-    <title> LOGIN</title>
+    <title>LOGIN</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Kanit:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="../Stylesheets/Login.css">
 </head>
 
@@ -18,18 +23,49 @@
 
 
                 <label class="uname"> User Name</label>
-                <input type="text" name="uname" placeholder="User Name"><br>
+                <input class="inputBox" type="text" name="uname" placeholder="User Name"><br>
                 <label class="pword">Password</label>
-                <input type="password" name="password" placeholder="Password"><br>
+                <input class="inputBox" type="password" name="password" placeholder="Password"><br>
 
                 <div class="button-container">
-                    <button class="forgot" type="button">Forgot pasword</button>
+                    <button class="forgot" type="button">Forgot password</button>
                     <button class="login" type="submit">Login</button>
                     <button class="register" type="button">Register</button>
                 </div>
             </form>
         </div>
     </div>
+
+    <!-- SVG code goes here -->
+    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+        width="100%" height="100%" viewBox="0 0 1600 900" preserveAspectRatio="xMidYMax slice">
+        <defs>
+            <linearGradient id="bg">
+                <stop offset="0%" style="stop-color:rgba(130, 158, 249, 0.06)"></stop>
+                <stop offset="50%" style="stop-color:rgba(76, 190, 255, 0.6)"></stop>
+                <stop offset="100%" style="stop-color:rgba(115, 209, 72, 0.2)"></stop>
+            </linearGradient>
+            <path id="wave" fill="url(#bg)" d="M-363.852,502.589c0,0,236.988-41.997,505.475,0
+    s371.981,38.998,575.971,0s293.985-39.278,505.474,5.859s493.475,48.368,716.963-4.995v560.106H-363.852V502.589z" />
+        </defs>
+        <g>
+            <use xlink:href='#wave' opacity=".3">
+                <animateTransform attributeName="transform" attributeType="XML" type="translate" dur="10s"
+                    calcMode="spline" values="270 230; -334 180; 270 230" keyTimes="0; .5; 1"
+                    keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0" repeatCount="indefinite" />
+            </use>
+            <use xlink:href='#wave' opacity=".6">
+                <animateTransform attributeName="transform" attributeType="XML" type="translate" dur="8s"
+                    calcMode="spline" values="-270 230;243 220;-270 230" keyTimes="0; .6; 1"
+                    keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0" repeatCount="indefinite" />
+            </use>
+            <use xlink:href='#wave' opacty=".9">
+                <animateTransform attributeName="transform" attributeType="XML" type="translate" dur="6s"
+                    calcMode="spline" values="0 230;-140 200;0 230" keyTimes="0; .4; 1"
+                    keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0" repeatCount="indefinite" />
+            </use>
+        </g>
+    </svg>
 </body>
 
 </html>
